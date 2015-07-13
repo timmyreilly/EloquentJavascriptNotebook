bb7db5566168425ac1a789f033ba7a60e941d414
timmyreilly <timreilly@live.com>
2015-07-13 13:52:50 -0700

    halfway through the chapter

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# EloquentJavascriptNotebook
+# EloquentJavaScriptNotebook
 Working through this book: http://eloquentjavascript.net/
 
 Notes.txt will contain the notes
+
 Main will contain the sample code
+
 They will change throughout the branches according to chapters.
--- main.js
+++ main.js
@@ -13,3 +13,55 @@ console.log(one + two);
 
 //alert("good morning");
 
+var x = 30;
+console.log("the value of x is", x);
+// -> The value of x is 30
+
+console.log(Math.max(2, 4));
+// -> 4
+
+console.log(Math.min(2,4) + 100);
+// -> 102
+
+//confirm("Shall we, then?");
+//prompt("Tell me everything you know.", "...");
+
+// var theNumber = Number(prompt("Pick a number", ""));
+// alert("Your number is the square root of " +
+//       theNumber * theNumber);
+// 	  
+// var theNumber = Number(prompt("Pick a number", ""));
+// if (!isNaN(theNumber))
+//   alert("Your number is the square root of " +
+//         theNumber * theNumber);
+// 
+// var theNumber = Number(prompt("Pick a number", ""));
+// if (!isNaN(theNumber))
+//   alert("Your number is the square root of " +
+//         theNumber * theNumber);
+// else
+//   alert("Hey. Why didn't you give me a number?");
+// var num = Number(prompt("Pick a number", "0"));
+// 
+// if (num < 10)
+//   alert("Small");
+// else if (num < 100)
+//   alert("Medium");
+// else
+//   alert("Large");
+
+var number = 0;
+while (number <= 12) {
+  console.log(number);
+  number = number + 2;
+}
+
+var result = 1;
+var counter = 0;
+while (counter < 10) {
+  result = result * 2;
+  counter = counter + 1;
+}
+console.log(result);
+// → 1024
+
