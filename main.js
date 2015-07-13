954bcc13058133d04a68a7afdb78b4d1146ced4d
timmyreilly <timreilly@live.com>
2015-07-13 13:37:08 -0700

    console.log is next

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 var x = 'foo';
 
 console.log(x);
+
+var luigisDebt = 140;
+luigisDebt = luigisDebt - 35;
+console.log(luigisDebt);
+// → 105
+
+
+var one = 1, two = 2;
+console.log(one + two);
+
+//alert("good morning");
+
