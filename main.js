b3fdd22712d0504da97bc476118e99194f8db644
timmyreilly <timreilly@live.com>
2015-07-13 16:06:21 -0700

    parameters and scopes

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
-var x = 'foo';
+var foo = 'foo';
+
+var square = function(x){
+  return x * x;
+};
+
+console.log(square(4));
+
+var makeNoise = function() {
+  console.log("PLING");
+}
+
+makeNoise();
+
+var power = function(base, exponent){
+  var result = 1;
+  for (var count = 0; count < exponent; count++){
+    result *= base;
+  }
+  return result;
+}
+
+console.log(power(2,10));
 
